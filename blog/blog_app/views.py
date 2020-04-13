d18e527f1cea08dc5bb260aed562347e0a6d6d65
sanix sandel <sanicksikani@gmail.com>
2020-04-13 21:36:45 +0300

    CRUD

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model=Post
@@ -8,4 +10,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields=['title', 'author', 'body'] 
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['title', 'body']   
+
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')  #     We use reverse_lazy as opposed to just reverse so that it won’t execute the URL
+#redirect until our view has finished deleting the blog post.
+
+
 # Create your views here.
