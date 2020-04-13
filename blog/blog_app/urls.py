8677b38a75b53bb253503479384278d7ddd15962
sanix sandel <sanicksikani@gmail.com>
2020-04-13 17:33:08 +0300

    initial commit

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns=[
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
